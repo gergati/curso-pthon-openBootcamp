9840bd2ee574b178a89ce867103bb7e995836cc9
german gatica <gergati1994@gmail.com>
2022-04-15 21:32:07 -0300

    Ejercicios de la clase 2

---
+++ 02-Interprete-de-python/ejercicio01.py
@@ -0,0 +1,10 @@
+name = "Mi nombre es german"
+print(name)
+print(type(name) is str)
+if(type(name is str )):
+    print("Es una cadena de texto")
+else:
+    print("No es una cadena de texto")
+
+name = "Ahora me llamo nicolas"
+print(name)
